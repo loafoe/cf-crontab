f14e4867de971f65be2fe16240289c6de98903fb
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-06-20 19:26:12 +0200

    Move State to crontab package

---
+++ crontab/state.go
@@ -0,0 +1,53 @@
+package crontab
+
+import (
+	"fmt"
+	"github.com/robfig/cron/v3"
+	"sync"
+)
+
+type State struct {
+	list []*Task
+	cronTab *cron.Cron
+	mux sync.Mutex
+}
+
+func (e *State)Entries() []*Task {
+	return e.list
+}
+
+func (e *State)StartCron() {
+	e.cronTab.Start()
+}
+
+func (e *State)AddEntries(newEntries []Task) {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+	for i, _ := range newEntries {
+		_ = newEntries[i].Add(e.cronTab)
+		e.list = append(e.list, &newEntries[i])
+	}
+}
+
+func (e *State) DeleteEntry(id int) error {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+	entryID := cron.EntryID(id)
+	for i, t := range e.Entries() {
+		if int(t.EntryID) == id {
+			fmt.Printf("Removing %d\n", id)
+			e.list = append(e.list[:i], e.list[i+1:]...)
+			e.cronTab.Remove(entryID)
+			return nil
+		}
+	}
+	return fmt.Errorf("entry not found: %d", id)
+}
+
+func NewState() *State {
+	state := &State{
+		cronTab: cron.New(cron.WithSeconds()),
+		list: make([]*Task, 0),
+	}
+	return state
+}
--- server/server.go
+++ server/server.go
@@ -3,15 +3,12 @@ package server
 import (
 	"crypto/subtle"
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"github.com/philips-labs/cf-crontab/crontab"
+	"github.com/spf13/viper"
 	"net/http"
 	"os"
 	"strconv"
-	"sync"
-
-	"github.com/labstack/echo/v4"
-	"github.com/robfig/cron/v3"
-	"github.com/spf13/viper"
 )
 
 // authCheck verifies basic auth. Username hardcoded to `redshift`
@@ -23,7 +20,7 @@ func authCheck(username, password string, c echo.Context) (bool, error) {
 	return false, nil
 }
 
-func entriesDeleteHandler(state *State) echo.HandlerFunc {
+func entriesDeleteHandler(state *crontab.State) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		stringID := c.Param("entryID")
 		entryID, err := strconv.Atoi(stringID)
@@ -38,13 +35,13 @@ func entriesDeleteHandler(state *State) echo.HandlerFunc {
 	}
 }
 
-func entriesGetHandler(state *State) echo.HandlerFunc {
+func entriesGetHandler(state *crontab.State) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return c.JSONPretty(http.StatusOK, state.Entries(), "  ")
 	}
 }
 
-func entriesPostHandler(state *State) echo.HandlerFunc {
+func entriesPostHandler(state *crontab.State) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		newEntries := []crontab.Task{}
 		if err := c.Bind(&newEntries); err != nil {
@@ -53,44 +50,6 @@ func entriesPostHandler(state *State) echo.HandlerFunc {
 		state.AddEntries(newEntries)
 		return c.JSONPretty(http.StatusOK, newEntries, "  ")
 	}
-}
-
-type State struct {
-	list []*crontab.Task
-	cronTab *cron.Cron
-	mux sync.Mutex
-}
-
-func (e *State)Entries() []*crontab.Task{
-	return e.list
-}
-
-func (e *State)StartCron() {
-	e.cronTab.Start()
-}
-
-func (e *State)AddEntries(newEntries []crontab.Task) {
-	e.mux.Lock()
-	defer e.mux.Unlock()
-	for i, _ := range newEntries {
-		_ = newEntries[i].Add(e.cronTab)
-		e.list = append(e.list, &newEntries[i])
-	}
-}
-
-func (e *State) DeleteEntry(id int) error {
-	e.mux.Lock()
-	defer e.mux.Unlock()
-	entryID := cron.EntryID(id)
-	for i, t := range e.Entries() {
-		if int(t.EntryID) == id {
-			fmt.Printf("Removing %d\n", id)
-			e.list = append(e.list[:i], e.list[i+1:]...)
-			e.cronTab.Remove(entryID)
-			return nil
-		}
-	}
-	return fmt.Errorf("entry not found: %d", id)
 }
 
 // Start starts the cf-crontab server
@@ -104,23 +63,21 @@ func Start() {
 	_ = viper.ReadInConfig()
 
 	// Cron
-	table := &State {
-		cronTab: cron.New(cron.WithSeconds()),
-		list: make([]*crontab.Task, 0),
-	}
+	state := crontab.NewState()
+
 	entries, err := crontab.LoadFromEnv()
 	if err != nil {
 		fmt.Printf("error loading config: %v\n", err)
 		return
 	}
-	table.AddEntries(entries)
-	table.StartCron()
+	state.AddEntries(entries)
+	state.StartCron()
 
 	// Echo
 	e := echo.New()
-	e.GET("/entries", entriesGetHandler(table))
-	e.POST("/entries", entriesPostHandler(table))
-	e.DELETE("/entries/:entryID", entriesDeleteHandler(table))
+	e.GET("/entries", entriesGetHandler(state))
+	e.POST("/entries", entriesPostHandler(state))
+	e.DELETE("/entries/:entryID", entriesDeleteHandler(state))
 	usePort := os.Getenv("PORT")
 	if usePort == "" {
 		usePort = "8080"
